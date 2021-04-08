22d3d3c4a8c9f874e97ceb44a68b6b98c709e83b
LImnaC <38061026+limnac@users.noreply.github.com>
2021-04-08 12:20:14 +0800

    Create ArrayQueue.java

---
+++ Queue/ArrayQueue.java
@@ -0,0 +1,27 @@
+public class ArrayQueue {
+    private int[] arr;
+    private int a;
+    private int b;
+    public ArrayQueue(){
+        arr = new int[10];
+        a = 0;
+        b = 0;
+    }
+    public void push(int x){
+        if((b+1)%arr.length!=a){
+            b = (b+1)%arr.length;
+            arr[b] = x;
+        }
+    }
+    public int pop(){
+        int tmp = b;
+        b = (b-1)%arr.length;
+        return arr[tmp];
+    }
+    public int peek(){
+        return arr[b];
+    }
+    public boolean isEmpty(){
+        return a==b;
+    }
+}
