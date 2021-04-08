134f4b2262baa1681acc899b1433d6674d2dfd44
LImnaC <38061026+limnac@users.noreply.github.com>
2021-04-08 10:49:20 +0800

    Create ListNode.java

---
+++ List/ListNode.java
@@ -0,0 +1,12 @@
+public class ListNode {
+    public int x;
+    public ListNode next;
+    public ListNode(){};
+    public ListNode(int x){
+        this.x = x;
+    }
+    public ListNode(int x,ListNode next){
+        this.x = x;
+        this.next = next;
+    }
+}
